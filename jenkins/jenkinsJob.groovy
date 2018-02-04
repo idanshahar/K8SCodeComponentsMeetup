6cc9c722c778744df932f6697e397644a7f5b251
Administrator <admin@example.com>
2018-02-04 23:39:23 +0200

    add helm to docker file & add jenkinsGroovy

--- jenkins/jenkinsJob.groovy
+++ jenkins/jenkinsJob.groovy
@@ -1,1 +1,30 @@
 
+node {
+    withCredentials([
+      [$class: 'UsernamePasswordMultiBinding', credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS'],
+    ]) {
+        stage('login') {
+           
+            sh """
+                docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+            """
+        }
+        stage('Preparation') {
+          git 'https://github.com/idanshahar/K8SCodeComponentsMeetup.git'
+        }
+        stage('Build') {
+        app = docker.build("idanshahar/node-example","-f ./node/Dockerfile ./node")
+        }
+        stage('Push') {
+            withDockerRegistry([credentialsId: 'docker-hub-credentials']) {
+                app.push("${BUILD_NUMBER}")
+                app.push("latest")
+            }
+        }
+        stage('Deploy') {
+            sh """
+                helm
+            """
+        }
+    }
+}
